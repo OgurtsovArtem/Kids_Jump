3748900ef6bd490794a2d8fe4ea1021ba96366a7
Ogurtsov Artem <ogurcov.artem2012@list.ru>
2022-02-21 18:35:05 +0300

    correction after changes in technicalClasses.scss

--- src/components/AdvantagesDescription/AdvantagesDescription.js
+++ src/components/AdvantagesDescription/AdvantagesDescription.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Navigation, Pagination } from 'swiper';
-import { Swiper, SwiperSlide, } from 'swiper/react';
+import React from "react";
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // Import components
-import AdvantagesCard from '../../Ui-Kit/AdvantagesCard/AdvantagesCard';
-import SliderControls from '../../Ui-Kit/SliderControls/SliderControls';
+import AdvantagesCard from "../../Ui-Kit/AdvantagesCard/AdvantagesCard";
+import SliderControls from "../../Ui-Kit/SliderControls/SliderControls";
 
 // Import style
-import "../../styles/technicalClasses.scss"
-import "./AdvantagesDescription.scss"
+import "../../styles/technicalClasses.scss";
+import "./AdvantagesDescription.scss";
 
 function AdvantagesTopSection({ className }) {
-  const [swiperActive, setSwiperActive] = React.useState(false)
+  const [swiperActive, setSwiperActive] = React.useState(false);
 
   const pagination = {
     clickable: true,
@@ -24,7 +24,7 @@ function AdvantagesTopSection({ className }) {
     el: ".advantages-description__pagination",
     currentClass: `swiper-pagination-current advantages__pagination`,
     totalClass: `swiper-pagination-total advantages__pagination`,
-  }
+  };
   const breakpoints = {
     0: {
       slidesPerView: 1,
@@ -43,40 +43,49 @@ function AdvantagesTopSection({ className }) {
       spaceBetween: 40,
     },
     1025: {
-      enabled: false
+      enabled: false,
     },
-  }
+  };
   const navigation = {
-    prevEl: '.advantages-description__button-left',
-    nextEl: '.advantages-description__button-right',
-  }
+    prevEl: ".advantages-description__button-left",
+    nextEl: ".advantages-description__button-right",
+  };
   React.useEffect(() => {
     const changeStatus = () => {
-      setSwiperActive(window.matchMedia("(max-width: 1024px)").matches)
-    }
-    window.addEventListener('resize', changeStatus);
-    return () => {
-      window.removeEventListener('resize', changeStatus);
+      setSwiperActive(window.matchMedia("(max-width: 1024px)").matches);
     };
-  })
+    window.addEventListener("resize", changeStatus);
+    return () => {
+      window.removeEventListener("resize", changeStatus);
+    };
+  });
 
   React.useEffect(() => {
-    setSwiperActive(window.matchMedia("(max-width: 1024px)").matches)
-  }, [])
+    setSwiperActive(window.matchMedia("(max-width: 1024px)").matches);
+  }, []);
 
   return (
     <div className={`${className} advantages-description`}>
       <div className="advantages-description__block">
-        <h2 className="advantages-description__title">Our <br /> Advantages</h2>
-        <p className="advantages-description__text"><b>KIDSjumpTECH</b> is part of the HighProject group of companies, which has implemented more than 3,000 interactive floor installation projects worldwide.</p>
-        <p className="advantages-description__text">Due to the large number of projects our team has identified all the current problems and needs of the market, so we offer our customers the most optimal equipment.</p>
+        <h2 className="advantages-description__title">
+          Our <br /> Advantages
+        </h2>
+        <p className="advantages-description__text">
+          <b>KIDSjumpTECH</b> is part of the HighProject group of companies,
+          which has implemented more than 3,000 interactive floor installation
+          projects worldwide.
+        </p>
+        <p className="advantages-description__text">
+          Due to the large number of projects our team has identified all the
+          current problems and needs of the market, so we offer our customers
+          the most optimal equipment.
+        </p>
       </div>
       <div className="advantages-description__cards">
-        {swiperActive
-          ?
+        {swiperActive ? (
           <>
             <Swiper
-              className="_swiper"
+              className="advantages-description__swiper"
               modules={[Navigation, Pagination]}
               spaceBetween={50}
               slidesPerView={1}
@@ -87,34 +96,57 @@ function AdvantagesTopSection({ className }) {
               breakpoints={breakpoints}
             >
               <SwiperSlide>
-                <AdvantagesCard className="advantages-description__card" iconName="launch-1" />
+                <AdvantagesCard
+                  className="advantages-description__card"
+                  iconName="launch-1"
+                />
               </SwiperSlide>
               <SwiperSlide>
-                <AdvantagesCard className="advantages-description__card" iconName="launch-1" />
+                <AdvantagesCard
+                  className="advantages-description__card"
+                  iconName="launch-1"
+                />
               </SwiperSlide>
               <SwiperSlide>
-                <AdvantagesCard className="advantages-description__card" iconName="launch-1" />
+                <AdvantagesCard
+                  className="advantages-description__card"
+                  iconName="launch-1"
+                />
               </SwiperSlide>
               <SwiperSlide>
-                <AdvantagesCard className="advantages-description__card" iconName="launch-1" />
+                <AdvantagesCard
+                  className="advantages-description__card"
+                  iconName="launch-1"
+                />
               </SwiperSlide>
-
             </Swiper>
             <SliderControls
               className="advantages-description__controls"
-              paginationClassName={pagination.el.replace(/\./g, '')}
-              leftButtonClassName={navigation.prevEl.replace(/\./g, '')}
-              rightButtonClassName={navigation.nextEl.replace(/\./g, '')}
+              paginationClassName={pagination.el.replace(/\./g, "")}
+              leftButtonClassName={navigation.prevEl.replace(/\./g, "")}
+              rightButtonClassName={navigation.nextEl.replace(/\./g, "")}
             />
           </>
-          :
+        ) : (
           <>
-            <AdvantagesCard className="advantages-description__card" iconName="launch-1" />
-            <AdvantagesCard className="advantages-description__card" iconName="launch-1" />
-            <AdvantagesCard className="advantages-description__card" iconName="launch-1" />
-            <AdvantagesCard className="advantages-description__card" iconName="launch-1" />
+            <AdvantagesCard
+              className="advantages-description__card"
+              iconName="launch-1"
+            />
+            <AdvantagesCard
+              className="advantages-description__card"
+              iconName="launch-1"
+            />
+            <AdvantagesCard
+              className="advantages-description__card"
+              iconName="launch-1"
+            />
+            <AdvantagesCard
+              className="advantages-description__card"
+              iconName="launch-1"
+            />
           </>
-        }
+        )}
       </div>
     </div>
   );
--- src/components/AdvantagesProducts/AdvantagesProducts.js
+++ src/components/AdvantagesProducts/AdvantagesProducts.js
@@ -1,46 +1,42 @@
-import React from 'react'
-import { Navigation, Pagination } from 'swiper';
-import { Swiper, SwiperSlide, } from 'swiper/react';
+import React from "react";
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // Import components
-import ViewButton from '../../Ui-Kit/ViewButton/ViewButton';
-import ArrowButtonMedium from '../../Ui-Kit/ArrowButtonMedium/ArrowButtonMedium';
-import AdvantagesCardProducts from '../../Ui-Kit/AdvantagesCardProducts/AdvantagesCardProducts';
+import ViewButton from "../../Ui-Kit/ViewButton/ViewButton";
+import ArrowButtonMedium from "../../Ui-Kit/ArrowButtonMedium/ArrowButtonMedium";
+import AdvantagesCardProducts from "../../Ui-Kit/AdvantagesCardProducts/AdvantagesCardProducts";
 // Import styles
 import "./AdvantagesProducts.scss";
 
 function AdvantagesProducts() {
-
   const breakpoints = {
     0: {
       spaceBetween: 10,
     },
     375: {
-
       spaceBetween: 20,
     },
     500: {
-
       spaceBetween: 30,
     },
     800: {
-
       spaceBetween: 40,
     },
     1025: {
       spaceBetween: 50,
     },
-  }
+  };
 
   const navigation = {
-    prevEl: '.advantages-products__button-left',
-    nextEl: '.advantages-products__button-right',
-  }
+    prevEl: ".advantages-products__button-left",
+    nextEl: ".advantages-products__button-right",
+  };
 
   return (
     <div className="advantages-products">
@@ -50,13 +46,19 @@ function AdvantagesProducts() {
             <span className="advantages-products__title-number">90+</span>
             <span className="advantages-products__title-text">Games</span>
           </h2>
-          <p className="advantages-products__text">More than 90 educational games and applications are included with the delivery of the interactive floor.</p>
+          <p className="advantages-products__text">
+            More than 90 educational games and applications are included with
+            the delivery of the interactive floor.
+          </p>
         </div>
-        <ViewButton className="advantages-products__view-button" text="view  all games" />
+        <ViewButton
+          className="advantages-products__view-button"
+          text="view  all games"
+        />
       </div>
-      <div className='advantages-products__cards'>
+      <div className="advantages-products__cards">
         <Swiper
-          className="_swiper _off-overflow _auto-width"
+          className="_swiper _off-overflow _auto-width advantages-products__swiper"
           modules={[Navigation, Pagination]}
           spaceBetween={50}
           slidesPerView={"auto"}
@@ -80,13 +82,13 @@ function AdvantagesProducts() {
             <AdvantagesCardProducts className="advantages-products__card" />
           </SwiperSlide>
         </Swiper>
-        <div className='advantages-products__buttons'>
+        <div className="advantages-products__buttons">
           <ArrowButtonMedium className="advantages-products__button-left" />
           <ArrowButtonMedium className="advantages-products__button-right" />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default AdvantagesProducts
+export default AdvantagesProducts;
